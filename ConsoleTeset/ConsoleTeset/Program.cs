533bd93e6dd3278d4b5f9eb07882bf2029e7da17
chandan.kesharwani <chandan.kesharwani@AKSMUMLA0203>
2021-08-17 16:47:38 +0530

    added 2 line

---
+++ ConsoleTeset/ConsoleTeset/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTeset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("New Repo!");
+        }
+    }
+}
